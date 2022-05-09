2077ae939fee9a6190a5c68d824eb321e9c92f81
«Artem <yevdeev@bk.ru>
2022-05-09 13:52:27 +0300

    8_l_1_2

---
+++ 8_L_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if ( a * a == b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
