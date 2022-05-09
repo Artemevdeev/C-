8adf4ee8aacf542becaf3f3c5df986094fc6ce7c
«Artem <yevdeev@bk.ru>
2022-05-09 16:44:33 +0300

    Нахождение модуля числа

---
+++ D_Z_01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+if(a >= 0)
+{
+     Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(a * (-1));
+}
