7fb348b6b19eabacce6f5929f0813d8591f5d708
«Artem <yevdeev@bk.ru>
2022-05-10 15:47:08 +0300

    Проверка числа на четность

---
+++ D_Z_04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int num = int.Parse(s);
+
+if(num%2==0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
