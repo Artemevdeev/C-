8d5e6baebff13cb5186d4ff6ddb8e511afea0d57
«Artem <yevdeev@bk.ru>
2022-05-10 14:40:15 +0300

    Выводим большее из двух чисел

---
+++ D_Z_02/Program.cs
@@ -0,0 +1,13 @@
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+if(a<b)
+{
+    Console.Write(b);
+}
+if(a>b)
+{
+    Console.Write(a);
+}
