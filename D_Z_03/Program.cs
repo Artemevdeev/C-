e781b96eb3ee97f3c42905842a6bc177f328c918
«Artem <yevdeev@bk.ru>
2022-05-10 15:22:59 +0300

    Выводим большее из трех чисел

---
+++ D_Z_03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s_b = Console.ReadLine();
+Console.WriteLine("Введите третье число: ");
+string s_c = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+
+if (b > a)
+{
+     a = b;
+if (c > a)
+{
+    a = c;
+}
+}
+Console.Write(" Max  "   + a );
+
+
