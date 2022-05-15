fd4e1fc918a34e613f712ed6e2258ca88fcaca61
«Artem <yevdeev@bk.ru>
2022-05-15 16:55:51 +0300

    программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа

---
+++ 2S_DZ_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int num1 = 0;
+int num2 = 0;
+
+void secondNumber( int num)
+{
+    Console.Write(num);
+    num1 = num/ 100;
+    num2 = num %10;
+    Console.WriteLine($" ==>  {num1}{num2}"); 
+    
+}
+secondNumber(new Random().Next(100,1000));
+secondNumber(new Random().Next(100,1000));
+secondNumber(new Random().Next(100,1000));
