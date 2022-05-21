36f4973bb23f31a28e65588ea509251a59f6d366
«Artem <yevdeev@bk.ru>
2022-05-21 23:44:21 +0300

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе

---
+++ 3L_DZ_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//  Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Sum(int num)
+{
+    int summ=0;
+    string len = num.ToString(); 
+    for(int j=0;j<len.Length;j++)
+    {
+        int num1 = num % 10;
+        num = num / 10;
+        summ= summ +num1;
+    }
+    return summ;
+}
+
+Console.WriteLine("Введите число");
+string isis=Console.ReadLine();
+int iss = int.Parse(isis);
+Console.WriteLine(Sum(iss));
