fd1fcbd5f857144a9c0977f22b04bd8865116557
«Artem <yevdeev@bk.ru>
2022-05-21 23:30:39 +0300

    апишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ 3L_DZ_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+ 
+
+ int СubeNum(int a, int b) // 2 и 3
+{
+    int all = a; // 2
+    {
+    for (int i = 1; i < b; i++)// i = 1 // i =2/ i = 3
+    {
+
+        all =  all * a; // 2 = 2*2 = 4,4 * 2= 8// 8 * 2 = 16 
+
+        
+    }
+    return all;
+
+    }
+}
+
+
+
+Console.WriteLine(СubeNum(2,1));
+Console.WriteLine(СubeNum(2,2));
+Console.WriteLine(СubeNum(2,3));
+Console.WriteLine(СubeNum(2,4));
+
+ 
+
+ 
