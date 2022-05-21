66f1754a3794a3fe0e2cfda6e34462249e4d899f
«Artem <yevdeev@bk.ru>
2022-05-22 00:23:47 +0300

    Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.

---
+++ 3L_DZ_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+
+Console.WriteLine("Введите числовое значение длинны массива");
+string isis=Console.ReadLine();
+int iss = int.Parse(isis);
+int[] arr = new int[iss];
+for (int i = 0; i < iss; i++)
+{
+    arr[i] = new Random().Next(1,100);        
+} 
+for(int j = 0; j < arr.Length; j++)
+{
+    Console.Write(arr[j].ToString() + " ");
+}
+
