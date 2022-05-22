66ef56cfed5f7fa7b4f063d7c6d286b88e467949
«Artem <yevdeev@bk.ru>
2022-05-22 14:33:54 +0300

    Программа показывает создает массив, и показывает сумму элементов на нечетных позициях

---
+++ 5L_DZ_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int [] GetArray(int length, int a, int b)
+{
+    int [] result = new int [length];
+    for (int i = 0 ; i< length ; i++)
+    {
+        result[i]= new Random().Next(a,b);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i=0; i<length; i++)
+    {
+        Console.Write($"{array[i]}   ");
+    }
+    
+    Console.WriteLine();
+}
+ void SumNechetPosit(int[] array)
+{
+int sum = 0;
+for (int i=0; i<array.Length; i+=2)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine(sum);
+}
+
+
+int [] array = GetArray(6,-10,10);
+PrintArray(array);
+SumNechetPosit(array);
