11e68cf02d53ce2e49cabe044731c1d6de8592ad
«Artem <yevdeev@bk.ru>
2022-05-22 13:56:18 +0300

    Программа показывает создает массив, и показывает кол-во четных элементов в массиве

---
+++ 5L_DZ_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] GetArray(int length, int a, int b)
+{
+    int [] result = new int [length];
+    for (int i = 0 ; i< length ; i++)
+    {
+        result[i]= new Random().Next(a,b);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i=0; i<length; i++)
+    {
+        Console.Write($"{array[i]}   ");
+    }
+    
+    Console.WriteLine();
+}
+
+void Сhetnost(int[] array)
+{
+int count = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]% 2 == 0) 
+    count ++;
+}
+Console.WriteLine(count);
+}
+
+int [] array = GetArray(6,10,200);
+PrintArray(array);
+Сhetnost(array);
