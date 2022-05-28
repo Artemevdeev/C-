94ba07b81d7fb70df5bca433ed567d014c7ec1a8
«Artem <yevdeev@bk.ru>
2022-05-28 22:38:30 +0300

    Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.

---
+++ 6L_DZ_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Введите колличество чисел");
+
+int num = int.Parse(Console.ReadLine());
+int count  = 0;
+for(int i=0; i<num;i++)
+{
+    Console.WriteLine("Введите число");
+    int num1 = int.Parse(Console.ReadLine());
+    if(num1>0) count++;
+}
+Console.WriteLine($"Кол-во положительных чисел {count}");
