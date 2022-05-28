e852a343c2a6d3fa24e4a38c080f39fa2e1b0091
«Artem <yevdeev@bk.ru>
2022-05-28 22:39:22 +0300

    Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями

---
+++ 6L_DZ_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = 2;
+double b2 = 4;
+double k1 = 5;
+double k2 = 9;
+
+void CoordinatePoint(double b1, double k1, double b2, double k2)
+{
+    double x = 0; double y = 0;
+    x = (b2 - b1)/(k1 - k2);
+    y = k1 * x + b1;
+  Console.WriteLine($" Точки пересечения {x}, {y}");
+}
+
+CoordinatePoint(b1, k1, b2, k2);
